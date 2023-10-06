926e261aa48bfb5674d6502f7f12beda1aea80f0
Yash Jagtap <jagtapyash2512@gmail.com>
2023-10-06 23:25:02 +0530

    prevention

---
+++ contracts/Prevention.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Prevention {
+    address public currentWinner;
+    uint public currentAuctionPrice;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        currentWinner = msg.sender;
+    }
+
+    function setCurrentAuctionPrice() public payable {
+        require(msg.value > currentAuctionPrice, "Need to pay more than the currentAuctionPrice");
+        balances[currentWinner] += currentAuctionPrice;
+        currentAuctionPrice = msg.value;
+        currentWinner = msg.sender;
+    }
+
+    function withdraw() public {
+        require(msg.sender != currentWinner, "Current winner cannot withdraw");
+
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
